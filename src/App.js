1916a5fcf736232e63cd804aa1fa43cf4566352b
Alexandre Leroux <alex@sherpa.one>
2022-05-19 18:16:50 +0200

    init

--- src/App.js
+++ src/App.js
@@ -1,23 +1,50 @@
-import logo from './logo.svg';
+import { v4 as uuid } from 'uuid';
 import './App.css';
+
+const data = [
+  {
+    id:uuid(),
+    completed:false,
+    content: "Sortir le chien",
+    date : "2022-05-15"
+  },  {
+    id:uuid(),
+    completed:false,
+    content: "Faire les courses",
+    date : "2022-05-19"
+  },  {
+    id:uuid(),
+    completed:false,
+    content: "Préparer à manger",
+    date : "2022-05-18"
+  },  {
+    id:uuid(),
+    completed:true,
+    content: "Acheter des fleurs à ma femme",
+    date : "2022-05-19"
+  },
+];
+
+function TaskPreview({task}){
+  return (
+    <article>
+      <p>{task.content}</p>
+    </article>
+  );
+}
+
+function TasksMaster({tasks}){
+  return (
+    <div>
+    {tasks.map(task=><TaskPreview key={task.id} task={task}/>)}
+    </div>
+  );
+}
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <TasksMaster tasks={data} />
     </div>
   );
 }
