2786b78e59ee4cd7168aa1af0ed2f4367488475d
Alexandre Leroux <alex@sherpa.one>
2022-05-24 14:29:30 +0200

    too much stuff

--- README.md
+++ README.md
@@ -1,4 +1,17 @@
-# Todolist Project Bootstrap
+# Todo List with React.js
+
+## Instructions
+
+### Env vars
+
+A root level, create a file called `.env` and insert :
+
+```
+NODE_ENV=production
+REACT_APP_ENDPOINT=https://<project-id>.directus.app/items/tasks/
+```
+
+### Development
 
 - First install NPM dependencies :
 
@@ -8,6 +21,81 @@ npm install
 
 - Then, start project :
 
-``
+```
 npm start
-``
+```
+
+### Production
+
+- build project :
+```
+npm build
+```
+
+## REST API
+
+Headless CMS
+
+https://directus.io/
+
+- Back Office : https://8s0fqfqq.directus.app/admin/login
+- API : https://8s0fqfqq.directus.app/items/tasks
+
+### Data Model
+
+#### Task
+
+- id : uuid v4 (36 chars),
+- completed : boolean (default false),
+- content : text,
+- created : datetime
+- updated : datetime
+
+Exemple :
+
+```
+{
+    "id":"28860c72-b13a-494e-b363-e1790df137b2",
+    "completed":true,
+    "created":"2022-05-24T09:24:00",
+    "content":"Learn and understand Redux",
+    "updated":"2022-05-24T09:41:34"
+}
+```
+
+### API Reference
+
+https://docs.directus.io/reference/introduction/
+
+#### Items
+
+https://docs.directus.io/reference/items/
+
+## Refactoring to do
+
+- merge EditTaskForm and AddTaskForm
+- create only one method "confirm_delete" (code duplication)
+- better deal API interactions
+- organize each component in separated files
+
+---
+
+__Alexandre Leroux__
+
+_Développeur logiciel full stack web & mobile_
+
+_Enseignant à l'Université de Lorraine_
+
+- _Institut des Sciences du Digital, Management & Cognition (Masters Sciences-Cognitives)_
+
+- _IUT Nancy-Charlemagne (Licence Professionnelle CIASIE)_
+
+<br>
+
+Website : https://sherpa.one
+
+Mail : alex@sherpa.one
+
+Github : sherpa1
+
+Discord : sherpa#3890
--- src/App.js
+++ src/App.js
@@ -1,22 +1,28 @@
-import { React, useState, useContext, createContext } from 'react';
+import { React, useState, useContext, useEffect, createContext } from 'react';
 import { useForm } from "react-hook-form";
 
 import { Route, Routes, Link, useLocation, useNavigate } from "react-router-dom";
 
+import axios from 'axios';
+
 import { v4 as uuid } from 'uuid';
+
 import './css/App.css';
-import './css/Form.css';
-import './css/icons.css';
 import './css/buttons.css';
+import './css/Form.css';
+import './css/Footer.css';
+import './css/Header.css';
+import './css/icons.css';
 import './css/TaskPreview.css';
+
+const ENDPOINT = process.env.REACT_APP_ENDPOINT;
 
 //context is a way to share state between multiple components like a local DB 
 const TasksContext = createContext(
   { tasks: [], setTasks: {} }
 );
 
-
-// components ending with "Screen" are dedicated to rooting
+// all components with name ending with "Screen" are only "containers" dedicated to rooting
 function HomeScreen() {
   return (
     <section>
@@ -25,32 +31,18 @@ function HomeScreen() {
   );
 }
 
+//Create a Task
 function AddScreen() {
-
   return (
     <section>
-      <h1>Add a new Task</h1>
+      <h2>Add a new Task</h2>
       <AddTaskForm />
     </section>
-
   );
+
 }
 
-function EditScreen() {
-
-  const location = useLocation();
-  const { task } = location.state;//get data from route state
-
-  return (
-    <section>
-      <h1>Edit Task</h1>
-      <h2>{task.id}</h2>
-
-      <EditTaskForm task={task} />
-    </section>
-  );
-}
-
+//Read a Task
 function SingleScreen() {
 
   const location = useLocation();
@@ -63,6 +55,23 @@ function SingleScreen() {
   );
 }
 
+//Update a Task
+function EditScreen() {
+
+  const location = useLocation();
+  const { task } = location.state;//get data from route state
+
+  return (
+    <section>
+      <h2>Edit Task</h2>
+      <h3>{task.id}</h3>
+
+      <EditTaskForm task={task} />
+    </section>
+  );
+}
+
+//Task Details (inside Single Screen)
 function TaskDetails({ task }) {
 
   const { tasks, setTasks } = useContext(TasksContext);
@@ -73,18 +82,40 @@ function TaskDetails({ task }) {
 
     if (delete_confirmation) {
       const tasks_without_deleted_task = tasks.filter(a_task => a_task.id !== id);
-      setTasks(tasks_without_deleted_task);
-      navigate('/tasks');
+
+      const result = await axios.delete(ENDPOINT + id);
+
+      if (result.status === 204) {
+
+        setTasks(tasks_without_deleted_task);
+        navigate('/tasks');
+
+      } else {
+        alert("Can't reach API");
+      }
+
     }
   }
 
   return (
     <div>
-      <h1>Task</h1>
-      <h2>{task.id}</h2>
-      <p>
-        Created at : {new Date(task.date).toLocaleString()}
-      </p>
+      <h2>Task</h2>
+      <h3>{task.id}</h3>
+      {
+        task.created ?
+          <p>
+            Created at : {new Date(task.created).toDateString()}
+          </p>
+          :
+          null
+      }
+      {
+        task.updated ?
+          <p>
+            Updated at : {new Date(task.updated).toDateString()}
+          </p>
+          : null
+      }
       <blockquote>{task.content}</blockquote>
       <p>
         Status : {task.completed ? 'Completed' : 'Not completed'}
@@ -92,11 +123,11 @@ function TaskDetails({ task }) {
 
       <footer>
         <Link to={`/tasks/${task.id}/edit`} state={{ task: task }}>
-          <button className='button button-outline'><span class="material-icons">edit</span>
+          <button className='button button-outline'><span className="material-icons">edit</span>
 
           </button>
         </Link>
-        <button className='button button-outline danger' onClick={() => confirm_delete(task.id)}><span class="material-icons">delete</span>
+        <button className='button button-outline danger' onClick={() => confirm_delete(task.id)}><span className="material-icons">delete</span>
 
         </button>
       </footer>
@@ -104,19 +135,29 @@ function TaskDetails({ task }) {
   );
 }
 
+//Task Preview (inside Task Master)
 function TaskPreview({ task }) {
 
   const { tasks, setTasks } = useContext(TasksContext);
   const navigate = useNavigate();
 
   async function confirm_delete(id) {
-
     const delete_confirmation = await window.confirm(`Do you really want to delete task "${id}" ?`);
 
     if (delete_confirmation) {
       const tasks_without_deleted_task = tasks.filter(a_task => a_task.id !== id);
-      setTasks(tasks_without_deleted_task);
-      navigate('/tasks');
+
+      const result = await axios.delete(ENDPOINT + id);
+
+      if (result.status === 204) {
+
+        setTasks(tasks_without_deleted_task);
+        navigate('/tasks');
+
+      } else {
+        alert("Can't reach API");
+      }
+
     }
   }
 
@@ -125,16 +166,16 @@ function TaskPreview({ task }) {
       <blockquote className={task.completed ? "completed" : null}>
         <Link to={`/tasks/${task.id}`} state={{ task: task }}>
           {task.content}
-          {task.completed ? <span class="material-icons green">check</span> : null}
+          {task.completed ? <span className="material-icons green">check</span> : null}
         </Link>
       </blockquote>
       <footer>
         <Link to={`/tasks/${task.id}/edit`} state={{ task: task }}>
-          <button className='button button-outline'><span class="material-icons">edit</span>
+          <button className='button button-outline'><span className="material-icons">edit</span>
 
           </button>
         </Link>
-        <button className='button button-outline danger' onClick={() => confirm_delete(task.id)}><span class="material-icons">delete</span>
+        <button className='button button-outline danger' onClick={() => confirm_delete(task.id)}><span className="material-icons">delete</span>
 
         </button>
       </footer>
@@ -142,13 +183,14 @@ function TaskPreview({ task }) {
   );
 }
 
+//Tasks list (inside Home Screen)
 function TasksMaster() {
 
   return (
     <TasksContext.Consumer>
       {({ tasks }) => (
         <div>
-          <h1>All Tasks</h1>
+          <h2>All Tasks</h2>
           {
             tasks.map(task => <TaskPreview key={task.id} task={task} />)
           }
@@ -162,18 +204,34 @@ function TasksMaster() {
   );
 }
 
+//Task Form (inside Add Screen)
 function AddTaskForm() {
   const { register, handleSubmit, formState: { errors } } = useForm();
   const { tasks, setTasks } = useContext(TasksContext);
   const navigate = useNavigate();
 
-  function on_submit(data) {
-    const { content } = data;
-    const newTask = { id: uuid(), completed: false, content: content, date: Date.now() };
+  async function on_submit(data) {
 
-    setTasks([...tasks, newTask]);
+    const { content } = data;//extracts "content" field value from submited data
 
-    navigate('/tasks');
+    const newTask = { id: uuid(), completed: false, content: content };
+
+    try {
+      const result = await axios.post(ENDPOINT, newTask);
+
+      if (result.status === 200 && result.statusText === "OK") {
+
+        setTasks([...tasks, newTask]);
+
+        navigate('/tasks');
+      } else {
+        alert("Can't reach API");
+      }
+
+    } catch (error) {
+      console.error(error);
+    }
+
   }
 
   return (
@@ -184,37 +242,50 @@ function AddTaskForm() {
           {errors.content && <span>Content is required</span>}
         </p>
 
-        {/* <input type="submit" value="Add" /> */}
-        <button className='button success'><span class="material-icons">check</span> Save</button>
+        <button className='button success'><span className="material-icons">check</span> <span>save</span></button>
       </form>
     </TasksContext.Provider>
   );
 }
 
+//Task Form (inside Edit Screen)
 function EditTaskForm({ task }) {
   const { register, handleSubmit, formState: { errors } } = useForm();
   const { tasks, setTasks } = useContext(TasksContext);
 
-  //deal the Task completed attr with a local state
-  const [completed, setCompleted] = useState(task.completed);
+  //deal the Task completed attr with a local state called "done"
+  const [done, setDone] = useState(task.completed);
 
   const navigate = useNavigate();
 
-  function on_submit(data) {
+  async function on_submit(data) {
     const { content } = data;
-    const updatedTask = { id: task.id, completed: completed, content: content, date: task.date };
+    const updatedTask = { id: task.id, completed: done, content: content };
 
     const index = tasks.findIndex(a_task => a_task.id === task.id);
     tasks[index] = updatedTask;
 
-    setTasks(tasks);
+    try {
+      const result = await axios.patch(ENDPOINT + updatedTask.id, updatedTask);
 
-    navigate('/tasks');
+      if (result.status === 200 && result.statusText === "OK") {
+
+        setTasks(tasks);
+
+        navigate('/tasks');
+
+      } else {
+        alert("Can't reach API");
+      }
+
+    } catch (error) {
+      console.error(error);
+    }
 
   }
 
   function on_click() {
-    setCompleted(completed ? false : true);
+    setDone(done ? false : true);
   }
 
   return (
@@ -226,42 +297,65 @@ function EditTaskForm({ task }) {
 
       <p>
         <label className='inline-label'>Completed ?</label>
-        <input onClick={() => on_click()} defaultChecked={completed ? "checked" : null} type="checkbox" />
+        <input onClick={() => on_click()} defaultChecked={done ? "checked" : null} type="checkbox" />
       </p>
 
-      <button className='button success'><span class="material-icons">check</span>
-
+      <button className='button edit'><span className="material-icons">update</span>
+        <span>update</span>
       </button>
     </form>
   );
 }
 
+//Main Component
 function App() {
 
   const [tasks, setTasks] = useState([]);
+
+  useEffect(() => {
+
+    async function fetch_tasks() {
+      const response = await axios.get(ENDPOINT);
+      setTasks(response.data.data);
+    }
+
+    fetch_tasks();
+
+  }, []);
 
   return (
     <TasksContext.Provider value={{ tasks, setTasks }}>
 
       <div className="App container">
-        <nav>
-          <ul>
-            <li>
-              <Link to="/tasks">All Tasks</Link>
-            </li>
-            <li>
-              <Link to="/tasks/add">Add a new Task</Link>
-            </li>
-          </ul>
-        </nav>
 
-        <Routes>
-          <Route path="/" element={<HomeScreen />} />
-          <Route path="/tasks" element={<HomeScreen />} />
-          <Route path="/tasks/add" element={<AddScreen />} />
-          <Route path="/tasks/:id" element={<SingleScreen />} />
-          <Route path="/tasks/:id/edit" element={<EditScreen />} />
-        </Routes>
+        <header>
+
+          <h1>Todo List</h1>
+
+          <Link to="/tasks">
+            <img src='/logo192.png' alt="logo" height="192px" />
+          </Link>
+
+          <nav>
+            <Link className='button' to="/tasks">All Tasks</Link>
+            <Link className='button' to="/tasks/add">Add a new Task</Link>
+          </nav>
+        </header>
+
+        <main>
+
+          <Routes>
+            <Route path="/" element={<HomeScreen />} />
+            <Route path="/tasks" element={<HomeScreen />} />
+            <Route path="/tasks/add" element={<AddScreen />} />
+            <Route path="/tasks/:id" element={<SingleScreen />} />
+            <Route path="/tasks/:id/edit" element={<EditScreen />} />
+          </Routes>
+        </main>
+
+        <footer>
+          <p>Todo List - Alexandre Leroux (alex@sherpa.one)</p>
+        </footer>
 
       </div>
     </TasksContext.Provider>
@@ -269,3 +363,8 @@ function App() {
 }
 
 export default App;
+
+//Todo : merge EditTaskForm and AddTaskForm
+//Todo : create only one method "confirm_delete" (code duplication)
+//Todo : better deal API interactions
+//Todo : organize each component in separated files
